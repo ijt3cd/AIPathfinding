6fa5ec5b7045dc37ec9a46af449bc4eec364705e
Isaac <ijt3cd@virginia.edu>
2016-06-23 22:33:32 +0200

    more progress

--- src/AIRobot.java
+++ src/AIRobot.java
@@ -7,16 +7,29 @@ import world.World;
 
 public class AIRobot extends Robot{
 
-	public void aStar(World w){
+	private World world;
+	
+	@Override
+	public void addToWorld(World world){
+		super.addToWorld(world);
+		this.world = world;
+	}
+	
+	public void aStar(){
 		//ping everything touching current point
 		//calculate value of each pinged point
 		//put each point onto an arrayList with value, in order
 		//pop the lowest value point onto the closed list and ping its neighbors
-		PriorityQueue<Node> openList = new PriorityQueue<Node>(w.numRows()*w.numCols());
+		PriorityQueue<Node> openNodeList = new PriorityQueue<Node>(world.numRows()*world.numCols());
+		ArrayList<Point> openPointList = new ArrayList<Point>();
 		ArrayList<Point> closedList = new ArrayList<Point>();
 		int distFromStart = 0;
 
+		
 		Point current = super.getPosition();
+		
+	for(int i = 0; i < 30; i++){
+	
 		Point t  = new Point(current.x,  current.y+1);
 		Point tr = new Point(current.x+1,current.y+1);
 		Point tl = new Point(current.x-1,current.y+1);
@@ -28,23 +41,57 @@ public class AIRobot extends Robot{
 		
 		++distFromStart;
 		
-		openList.add(new Node(t,  distFromStart, hVal(t, w)));
-		openList.add(new Node(tr, distFromStart, hVal(tr,w)));
-		openList.add(new Node(tl, distFromStart, hVal(tl,w)));
-		openList.add(new Node(r,  distFromStart, hVal(r, w)));
-		openList.add(new Node(l,  distFromStart, hVal(l, w)));
-		openList.add(new Node(b,  distFromStart, hVal(b, w)));
-		openList.add(new Node(br, distFromStart, hVal(br,w)));
-		openList.add(new Node(bl, distFromStart, hVal(bl,w)));
+		if(!closedList.contains(t)&&!openPointList.contains(t)){
+		openNodeList.add(new Node(t,  distFromStart, hVal(t)));
+		openPointList.add(t);
+		}
+		if(!closedList.contains(tr)&&!openPointList.contains(tr)){
+		openNodeList.add(new Node(tr, distFromStart, hVal(tr)));
+		openPointList.add(tr);
+		}
+		if(!closedList.contains(tl)&&!openPointList.contains(tl)){
+		openNodeList.add(new Node(tl, distFromStart, hVal(tl)));
+		openPointList.add(tl);
+		}
+		if(!closedList.contains(r)&&!openPointList.contains(r)){
+		openNodeList.add(new Node(r,  distFromStart, hVal(r)));
+		openPointList.add(r);
+		}
+		if(!closedList.contains(l)&&!openPointList.contains(l)){
+		openNodeList.add(new Node(l,  distFromStart, hVal(l)));
+		openPointList.add(l);
+		}
+		if(!closedList.contains(b)&&!openPointList.contains(b)){
+		openNodeList.add(new Node(b,  distFromStart, hVal(b)));
+		openPointList.add(b);
+		}
+		if(!closedList.contains(br)&&!openPointList.contains(br)){
+		openNodeList.add(new Node(br, distFromStart, hVal(br)));
+		openPointList.add(br);
+		}
+		if(!closedList.contains(bl)&&!openPointList.contains(bl)){
+		openNodeList.add(new Node(bl, distFromStart, hVal(bl)));
+		openPointList.add(bl);
+		}
 		
-
+		Node check = openNodeList.poll();
+	
+		current=check.getPt();
+		
+		closedList.add(check.getPt());
+		distFromStart=check.getDistFromStart();
+		System.out.println(check.getPt());
+//		System.out.println(check.getValue());
+//		System.out.println(super.pingMap(check.getPt()));
+//		System.out.println(openNodeList.size());
+	}
 		
 		
 	}
 	
-	public int hVal(Point p, World w){
-		int xDist = Math.abs(p.x-w.getEndPos().x);
-		int yDist = Math.abs(p.y-w.getEndPos().y);
+	public int hVal(Point p){
+		int xDist = Math.abs(p.x-world.getEndPos().x);
+		int yDist = Math.abs(p.y-world.getEndPos().y);
 		return Math.max(xDist, yDist);
 	}
 	
@@ -53,9 +100,11 @@ public class AIRobot extends Robot{
 	
 	@Override
 	public void travelToDestination() {
-		System.out.println(super.pingMap(new Point(-1,-1)));
-		System.out.println(super.pingMap(new Point(3,3)));
-		super.move(new Point(3,3));
+//		super.pingMap(new Point(-1,-1));
+//		super.pingMap(new Point(3,3));
+//		super.move(new Point(3,3));
+//		
+		aStar();
 		
 		
 
@@ -71,7 +120,6 @@ public class AIRobot extends Robot{
 		wo.createGUI(600, 600, 100);
 		AIRobot aiRobot = new AIRobot();
 		aiRobot.addToWorld(wo);
-		System.out.println(wo.getEndPos());
 		aiRobot.travelToDestination();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
